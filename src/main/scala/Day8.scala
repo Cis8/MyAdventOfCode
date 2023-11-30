606330fdd4baf759be153a448c38da16263e24e3
Emanuele Cisotto <emanuele.cisotto@kynetics.com>
2023-11-30 18:01:47 +0100

    Tmp commit

--- src/main/scala/Day8.scala
+++ src/main/scala/Day8.scala
@@ -1,27 +1,50 @@
 package ema.cis8
 
+import scala.io.Source
+import scala.util.Using
+
 object Day8 {
   @main def part1(): Unit = {
-
+    println(System.getProperty("user.dir"))
+    val forest = parse()
+    println(printMatrix(forest))
+    println(s"${forest.rows} x ${forest.columns}")
   }
 
-  def parse(): Nothing = ???
+  def parse(): Forest = {
+    Using(Source.fromFile("./src/main/resources/day8.txt")) {
+      src =>
+        src.getLines()
+          .map(r => r.toCharArray
+            .map(v => (v.toInt - 48, false)))
+          .toArray
+    }.get
+  }
 
-  private type Tree = Int
+  private def printMatrix[A](m: Array[Array[A]]) = m.map(r => r.mkString("", ", ", "")).mkString("", "\n", "")
+
+  private type Tree = (Int, Boolean)
+
+  extension (t: Tree)
+    def show = s"(${t._1},${t._2})"
 
   private type Forest = Array[Array[Tree]]
 
   extension (f: Forest)
-    def columns: Int = f.length
-    //def rows: Int = f[0].length
+    private def columns: Int = f.length
+    private def rows: Int = f(0).length
 
-  private type VisibilityMap = Array[Array[Boolean]]
+  def negativeOnes(n: Int) = Array.fill(n) {
+    -1
+  }
 
-  class RightDownScan(
-                       val forest: Forest,
-                       val visibilityMap: VisibilityMap) {
-    val rowTallestTree: Array[Tree] = Array.fill(forest.length) { -1 }
-    val columnTallestTree: Array[Tree] = Array.fill(forest.length) { -1 }
+  def rightDownScan(forest: Forest) = {
+    val columnsTallestTree = negativeOnes(forest.columns)
+    val columnsTallestColumns = negativeOnes(forest.rows)
+
+    forest.map(row => row.zipWithIndex.map((t, idx) => t match
+      case (_, false) => if t._1 >=
+    ))
   }
 
   class LeftUpScan(val forest: Forest)
